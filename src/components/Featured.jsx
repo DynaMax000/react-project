9f7b1c71afe2dd4a8235abe1f9535dddec5c0fca
Lavesh Kartik <laveshkartick1234@gmail.com>
2025-07-28 00:25:46 +0530

    Enhances Featured component with dynamic text overlays and improved layout

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -10,8 +10,15 @@ function Featured() {
       <div className='cards px-20 mt-20 flex gap-5 w-full'>
         <div className='w-1/2'>
           <h1 className='text-white text-[3vh] tracking-tight font-["Neue_Montreal"] mb-7'>• Salience Labs</h1>
-          <div className='cardcontainer h-[75vh]'>
-            <div className='rounded-xl overflow-hidden  w-full h-full'>
+          <div className='cardcontainer relative h-[75vh]'>
+            <h1 className='absolute left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] font-["Founders_Grotesk"] text-[20vh] z-[9] tracking tighter leading-none'>
+              {"Branding".split("").map((char, index) => {
+                return (
+                  <span>{char}</span>
+                );
+              })}
+            </h1>
+            <div className='rounded-xl overflow-hidden w-full h-full'>
               <img className='w-full h-full bg-cover' src='https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png'></img>
             </div>
             <div className='flex gap-3'>
@@ -24,7 +31,14 @@ function Featured() {
         <div className='w-1/2'>
           <h1 className='text-white text-[3vh] tracking-tight font-["Neue_Montreal"] mb-7'>• Cardboard Spaceships</h1>
 
-          <div className='cardcontainer h-[75vh]'>
+          <div className='cardcontainer relative h-[75vh]'>
+            <h1 className='absolute right-full translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] font-["Founders_Grotesk"] text-[20vh] z-[9] tracking tighter leading-none'>
+              {"Double".split("").map((char, index) => {
+                return (
+                  <span>{char}</span>
+                );
+              })}
+            </h1>
             <div className='rounded-xl overflow-hidden  w-full h-full'>
               <img className='w-full h-full bg-cover' src='https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-1326x1101.png'></img>
             </div>
