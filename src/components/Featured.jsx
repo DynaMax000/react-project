9aa6e54dc8e1f64f81cb182512efb206968e9f48
Lavesh Kartik <laveshkartick1234@gmail.com>
2025-07-31 23:30:58 +0530

    Enhances Featured component with GSAP animations and hover effects for dynamic text display

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,6 +1,10 @@
+import { motion } from 'framer-motion';
 import React from 'react'
+import { useState } from 'react';
+import { Power4 } from 'gsap';
 
 function Featured() {
+  const [isHovering, setHovering] = useState(false);
   return (
     <div className='w-full py-20'>
       <div className='w-full px-20 border-b-2 pb-10 border-zinc-600'>
@@ -10,18 +14,24 @@ function Featured() {
       <div className='cards px-20 mt-20 flex gap-5 w-full'>
         <div className='w-1/2'>
           <h1 className='text-white text-[3vh] tracking-tight font-["Neue_Montreal"] mb-7'>• Salience Labs</h1>
-          <div className='cardcontainer relative h-[75vh]'>
-            <h1 className='absolute left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] font-["Founders_Grotesk"] text-[20vh] z-[9] tracking tighter leading-none'>
+          <div onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)} className='cardcontainer relative h-[75vh]'>
+            <h1 className='absolute flex left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] font-["Neue_Montreal"]  text-[20vh] z-[9] tracking-tighter overflow-hidden'>
               {"Branding".split("").map((char, index) => {
                 return (
-                  <span>{char}</span>
+                  <motion.span
+                    initial={{ y: "100%" }}
+                    animate={isHovering ? ({ y: "0" }) : ({ y: "100%" })}
+                    transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .06 }}
+                    className='inline-block'>
+                    {char}
+                  </motion.span>
                 );
               })}
             </h1>
             <div className='rounded-xl overflow-hidden w-full h-full'>
               <img className='w-full h-full bg-cover' src='https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png'></img>
             </div>
-            <div className='flex gap-3'>
+            <div className='flex gap-3 text-black'>
               <button type='submit' className='bg-white px-3 py-1 border-2 border-black uppercase mt-3 rounded-full'>brand identity</button>
 
               <button type='submit' className='bg-white px-3 py-1 border-2 border-black uppercase mt-3 rounded-full'>pitch deck</button>
@@ -32,10 +42,16 @@ function Featured() {
           <h1 className='text-white text-[3vh] tracking-tight font-["Neue_Montreal"] mb-7'>• Cardboard Spaceships</h1>
 
           <div className='cardcontainer relative h-[75vh]'>
-            <h1 className='absolute right-full translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] font-["Founders_Grotesk"] text-[20vh] z-[9] tracking tighter leading-none'>
+            <h1 className='absolute flex right-full translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] font-["Neue_Montreal"] text-[20vh] z-[9] tracking tighter overflow-hidden'>
               {"Double".split("").map((char, index) => {
                 return (
-                  <span>{char}</span>
+                  <motion.span
+                    initial={{ y: "100%" }}
+                    animate={isHovering ? ({ y: "0" }) : ({ y: "100%" })}
+                    transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .06 }}
+                    className='inline-block'>
+                    {char}
+                  </motion.span>
                 );
               })}
             </h1>
@@ -43,7 +59,7 @@ function Featured() {
               <img className='w-full h-full bg-cover' src='https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-1326x1101.png'></img>
             </div>
           </div>
-          <div className='flex gap-3'>
+          <div className='flex gap-3 text-black'>
             <button type='submit' className='bg-white px-3 py-1 border-2 border-black uppercase mt-3 rounded-full'>branded template</button>
             <button type='submit' className='bg-white px-3 py-1 border-2 border-black uppercase mt-3 rounded-full'>sales deck</button>
             <button type='submit' className='bg-white px-3 py-1 border-2 border-black uppercase mt-3 rounded-full'>social media templates</button>
